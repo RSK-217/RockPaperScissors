8b66aeaff8f39f2ae7c52d26902ffcb74c3e4b94
Rick Kersmarki <rkersmarki@gmail.com>
2022-07-19 23:44:09 -0500

    Add project files.

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+Console.WriteLine(@"What would you like to throw?
+1. Rock
+2. Paper
+3. Scissors");
+string playerChoice = Console.ReadLine();
+Console.WriteLine(playerChoice);
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
---
+++ RockPaperScissors/Shoot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors;
+
+internal class Shoot
+{
+    public int Rock { get; set; }
+    public int Paper { get; set; }
+    public int Scissors { get; set; }
+
+    public int Points { get; set; }
+}
